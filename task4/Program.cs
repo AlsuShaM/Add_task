3a583239597a805739952f91f639200a3911830b
Alsu <shakilsu@yandex.ru>
2022-11-20 23:02:58 +0300

    task4

---
+++ task4/Program.cs
@@ -0,0 +1,12 @@
+﻿// Найти максимальное из трех чисел
+
+int a = 3;
+int b = 5;
+int c = 1;
+int max = a;
+
+if(b > max) max = b;
+if(c > max) max = c;
+
+Console.Write("max = ");
+Console.WriteLine(max);
