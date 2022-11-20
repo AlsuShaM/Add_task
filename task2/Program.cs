5b925cde4a3bf8ee3491de17c032d50ec7bf4810
Alsu <shakilsu@yandex.ru>
2022-11-20 23:00:03 +0300

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Даны два числа. Показать большее и меньшее число
+
+int a = 9;
+int b = 5;
+int max = a;
+int min = b;
+
+if(a > b)
+{
+    Console.WriteLine("max=a, min=b");
+}
+else
+{
+Console.WriteLine("max=b, min=a");
+}
